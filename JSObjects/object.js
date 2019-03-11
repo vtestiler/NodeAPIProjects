5c7e54a106403868063e784f885c05dacb628033
vladimir testiler <vtestiler@yahoo.com>
2019-03-11 14:30:53 -0400

    1st Commit

---
+++ JSObjects/object.js
@@ -0,0 +1,28 @@
+// Object literal function
+// var truck = {
+// make: 'Honda',
+// color: 'Green',
+// model: 'Titan',
+//
+// truckInfo: function(){
+//   return ` ${this.make} ${this.model} ${this.color}`;
+// }
+// };
+//
+// console.log(truck.truckInfo());
+
+// Object constructor function
+function Car (make, model, year){
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.getFullCarName = function(){
+    return `${this.make} ${this.model} ${this.year}`;
+  };
+}
+let myCar = new Car('BMW', '328 Xi', 2010);
+let myOldCar = new Car ('Honda', 'Accord Ex', 1997);
+let myWifesCar = new Car ('Nissan', 'Murano SL', 2017);
+let myWifesOldCar = new Car ('Toyota', 'Highlander ES', 2003)
+
+console.log(myWifesOldCar.getFullCarName());
